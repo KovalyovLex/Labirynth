e48316ee564e46484087da4c4c7384a9bedac7a2
Alexander Kovalyov <kovalyov.lex@gmail.com>
2012-05-18 11:50:26 +0400

    scale of choose game

--- labirynth/gen/com/flexymind/labirynth/R.java
+++ labirynth/gen/com/flexymind/labirynth/R.java
@@ -27,20 +27,19 @@ public final class R {
         public static final int stenka2=0x7f02000d;
     }
     public static final class id {
-        public static final int ExitButton=0x7f06000d;
-        public static final int SettingsButton=0x7f06000c;
-        public static final int StartButton=0x7f06000b;
-        public static final int buttLayout=0x7f06000a;
-        public static final int buttonLayout=0x7f060001;
+        public static final int ExitButton=0x7f06000c;
+        public static final int SettingsButton=0x7f06000b;
+        public static final int StartButton=0x7f06000a;
+        public static final int buttLayout=0x7f060009;
         public static final int choicemainlayout=0x7f060000;
-        public static final int chosebuttonlayout=0x7f060004;
-        public static final int game=0x7f060008;
-        public static final int gameLayout=0x7f060007;
-        public static final int horizontalScrollView1=0x7f060002;
-        public static final int imageLevel1=0x7f060006;
-        public static final int levelsLayout=0x7f060003;
-        public static final int nameleveltext=0x7f060005;
-        public static final int startmainlayout=0x7f060009;
+        public static final int chosebuttonlayout=0x7f060003;
+        public static final int game=0x7f060007;
+        public static final int gameLayout=0x7f060006;
+        public static final int horizontalScrollView1=0x7f060001;
+        public static final int imageLevel1=0x7f060005;
+        public static final int levelsLayout=0x7f060002;
+        public static final int nameleveltext=0x7f060004;
+        public static final int startmainlayout=0x7f060008;
     }
     public static final class layout {
         public static final int choicelevel=0x7f030000;
--- labirynth/src/com/flexymind/labirynth/screen/choicelevel/ChoiceLevelScreen.java
+++ labirynth/src/com/flexymind/labirynth/screen/choicelevel/ChoiceLevelScreen.java
@@ -10,16 +10,12 @@ import com.flexymind.labirynth.storage.LevelStorage;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Configuration;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Display;
 import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -30,6 +26,7 @@ public class ChoiceLevelScreen extends Activity implements OnClickListener{
 
 	private static final int id = 2376;
 	private Vector<String> names = null;
+	private LevelStorage lvlstor = null;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -40,50 +37,11 @@ public class ChoiceLevelScreen extends Activity implements OnClickListener{
 
 		ScreenSettings.GenerateSettings(display.getWidth(), display.getHeight());
 		
-		LevelStorage lvlstor = new LevelStorage(this);
+		lvlstor = new LevelStorage(this);
 		
 		names = lvlstor.get_level_names();
 		
-		LinearLayout levelslay = (LinearLayout)findViewById(R.id.levelsLayout);
-		
-		TextView newnameOfLvl;
-		ImageButton newbutton;
-		LinearLayout newbulllay;
-		
-		levelslay.removeAllViews();
-		
-		for (int i = 0; i < names.size(); i++){
-			newnameOfLvl = new TextView(this);
-			newbutton = new ImageButton(this);
-			newbulllay = new LinearLayout(this);
-			
-			newbulllay.setOrientation(LinearLayout.VERTICAL);
-			
-			Boolean free = new Boolean (lvlstor.isFree(names.get(i)));
-			Log.v("Free", names.get(i) + " " + free.toString() );
-			
-			newbutton.setEnabled(lvlstor.isFree(names.get(i)));
-			newbutton.setId(id + i);
-			newbutton.setOnClickListener(this);
-			newbutton.setBackgroundDrawable(lvlstor.getPrevPictireByName(names.get(i)));
-			
-			RelativeLayout.LayoutParams buttparams = new RelativeLayout.LayoutParams(220, 220);
-			
-			newnameOfLvl.setText(names.get(i));
-			newnameOfLvl.setGravity(Gravity.CENTER_HORIZONTAL);
-			
-			newbulllay.removeAllViews();
-			newbulllay.addView(newnameOfLvl);
-			newbulllay.addView(newbutton,buttparams);
-			
-			levelslay.addView(newbulllay);
-		}
-		
-		// Кнопка "Start"
-		//Button startButton = (Button) findViewById(R.id.StartChosenButton);
-		//startButton.setOnClickListener(this);
-		
-		AutoSize();
+		addButtons();
 	}
 			
 	/** Обработка нажатия кнопок */
@@ -101,87 +59,63 @@ public class ChoiceLevelScreen extends Activity implements OnClickListener{
 		super.onConfigurationChanged(newConfig);
 	}
 		
-	public void AutoSize() {
-		if (ScreenSettings.AutoScale()) {
-			
-			// высота и ширина кнопок на экране 480x800
-			int heightButton = 70;
-			int widthButton = 240;
-			int heightImageBut = 150;
-			int widthImageBut = 220;
-			int newX;
-		    int newY;
-				
-			LinearLayout buttonLayout = (LinearLayout)findViewById(R.id.buttonLayout);
-			LinearLayout imageLayout = (LinearLayout)findViewById(R.id.levelsLayout);
-				
-			RelativeLayout.LayoutParams butLLparams = (RelativeLayout.LayoutParams)buttonLayout.getLayoutParams();
-			butLLparams.topMargin = (int)(ScreenSettings.ScaleFactorY() * butLLparams.topMargin);
-			butLLparams.bottomMargin = (int)(ScreenSettings.ScaleFactorY() * butLLparams.bottomMargin);
-			butLLparams.leftMargin = (int)(ScreenSettings.ScaleFactorX() * butLLparams.leftMargin);
-			butLLparams.rightMargin = (int)(ScreenSettings.ScaleFactorX() * butLLparams.rightMargin);
-				
-			RelativeLayout.LayoutParams imageLLparams = (RelativeLayout.LayoutParams)imageLayout.getLayoutParams();
-			imageLLparams.topMargin = (int)(ScreenSettings.ScaleFactorY() * imageLLparams.topMargin);
-			imageLLparams.bottomMargin = (int)(ScreenSettings.ScaleFactorY() * imageLLparams.bottomMargin);
-			imageLLparams.leftMargin = (int)(ScreenSettings.ScaleFactorX() * imageLLparams.leftMargin);
-			imageLLparams.rightMargin = (int)(ScreenSettings.ScaleFactorX() * imageLLparams.rightMargin);
-				
-			//resizeLayout(buttonLayout);
-			//resizeLayout(imageLayout);
-				
-			RelativeLayout mainLayout = (RelativeLayout)findViewById(R.id.choicemainlayout);
-				
-			// Кнопка "Start"
-			//Button startButton = (Button) findViewById(R.id.StartChosenButton);
+	public void addButtons() {
+		// высота и ширина кнопок на экране 480x800
+		int heightImageBut = 220;
+		int widthImageBut = 220;
 		
-			ImageButton level1ImageBut = (ImageButton)findViewById(R.id.imageLevel1);
-			//ImageButton level2ImageBut = (ImageButton)findViewById(R.id.imageLevel2);
-			//ImageButton level3ImageBut = (ImageButton)findViewById(R.id.imageLevel3);
-				
-			// удаляем все кнопки				
-			buttonLayout.removeAllViews();	
-			imageLayout.removeAllViews();	
-				
-			RelativeLayout.LayoutParams paramsButton = new RelativeLayout.LayoutParams( (int)(ScreenSettings.ScaleFactorX() * widthButton), 
-																							(int)(ScreenSettings.ScaleFactorY() * heightButton) );
-			RelativeLayout.LayoutParams paramsImageBut = new RelativeLayout.LayoutParams( (int)/*(ScreenSettings.ScaleFactorX() */ widthImageBut,
-																							  (int)/*(ScreenSettings.ScaleFactorY() */ heightImageBut );
-				
-			Bitmap tmp=((BitmapDrawable)level1ImageBut.getDrawable()).getBitmap();
-			newX = (int)(ScreenSettings.ScaleFactorX() * tmp.getWidth());
-		    newY = (int)(ScreenSettings.ScaleFactorY() * tmp.getHeight());
-		    Bitmap bmp = Bitmap.createScaledBitmap(tmp, newX, newY, true);
-			level1ImageBut.setImageDrawable(new BitmapDrawable(bmp));
-				
-			//tmp=((BitmapDrawable)level2ImageBut.getDrawable()).getBitmap();
-		    //bmp = Bitmap.createScaledBitmap(tmp, newX, newY, true);
-		    //level2ImageBut.setImageDrawable(new BitmapDrawable(bmp));
-				
-		    //tmp=((BitmapDrawable)level3ImageBut.getDrawable()).getBitmap();
-		    //bmp = Bitmap.createScaledBitmap(tmp, newX, newY, true);
-		    //level3ImageBut.setImageDrawable(new BitmapDrawable(bmp));
-				
-			//buttonLayout.addView(startButton, paramsButton);
-			//imageLayout.addView(level1ImageBut, paramsImageBut);
-			//imageLayout.addView(level2ImageBut, paramsImageBut);
-			//imageLayout.addView(level3ImageBut, paramsImageBut);
-				
-			// удаляем layout с кнопками
-			mainLayout.removeAllViews();
-				
-			mainLayout.addView(buttonLayout, butLLparams);
-			mainLayout.addView(imageLayout, imageLLparams);	
+		LinearLayout levelslay = (LinearLayout)findViewById(R.id.levelsLayout);
+		
+		TextView newnameOfLvl;
+		ImageButton newbutton;
+		LinearLayout newbulllay;
+		
+		levelslay.removeAllViews();
+	    
+		if (ScreenSettings.AutoScale()) {
+			heightImageBut = (int)(heightImageBut * ScreenSettings.ScaleFactorY());
+			widthImageBut = (int)(widthImageBut * ScreenSettings.ScaleFactorX());
+			
+			HorizontalScrollView hscroll = (HorizontalScrollView)findViewById(R.id.horizontalScrollView1);
+			
+			RelativeLayout parent = (RelativeLayout)hscroll.getParent();
+			
+			parent.removeAllViews();
+			
+			RelativeLayout.LayoutParams rlparams = (RelativeLayout.LayoutParams)hscroll.getLayoutParams();
+			rlparams.topMargin = (int)(ScreenSettings.ScaleFactorY() * rlparams.topMargin);
+			rlparams.bottomMargin = (int)(ScreenSettings.ScaleFactorY() * rlparams.bottomMargin);
+			rlparams.leftMargin = (int)(ScreenSettings.ScaleFactorX() * rlparams.leftMargin);
+			rlparams.rightMargin = (int)(ScreenSettings.ScaleFactorX() * rlparams.rightMargin);
+			rlparams.height = (int)(ScreenSettings.ScaleFactorY() * rlparams.height);
+			rlparams.width = (int)(ScreenSettings.ScaleFactorX() * rlparams.width);
+			
+			parent.addView(hscroll,rlparams);
 		}
 
-	}
-		
-	public void resizeLayout(LinearLayout llayout){
-		RelativeLayout.LayoutParams llparams = (RelativeLayout.LayoutParams)llayout.getLayoutParams();
-		llparams.topMargin = (int)(ScreenSettings.ScaleFactorY() * llparams.topMargin);
-		llparams.bottomMargin = (int)(ScreenSettings.ScaleFactorY() * llparams.bottomMargin);
-		llparams.leftMargin = (int)(ScreenSettings.ScaleFactorX() * llparams.leftMargin);
-		llparams.rightMargin = (int)(ScreenSettings.ScaleFactorX() * llparams.rightMargin);
+		for (int i = 0; i < names.size(); i++){
+			newnameOfLvl = new TextView(this);
+			newbutton = new ImageButton(this);
+			newbulllay = new LinearLayout(this);
+			
+			newbulllay.setOrientation(LinearLayout.VERTICAL);
+			
+			newbutton.setEnabled(lvlstor.isFree(names.get(i)));
+			newbutton.setId(id + i);
+			newbutton.setOnClickListener(this);
+			newbutton.setBackgroundDrawable(lvlstor.getPrevPictireByName(names.get(i)));
+			
+			RelativeLayout.LayoutParams buttparams = new RelativeLayout.LayoutParams(widthImageBut, heightImageBut);
+			
+			newnameOfLvl.setText(names.get(i));
+			newnameOfLvl.setGravity(Gravity.CENTER_HORIZONTAL);
+			
+			newbulllay.removeAllViews();
+			newbulllay.addView(newnameOfLvl);
+			newbulllay.addView(newbutton,buttparams);
+			
+			levelslay.addView(newbulllay);
+		}
 	}
 
 }
